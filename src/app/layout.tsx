77fee0268eba3005374f89fbb95f103f1fcf7c66
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2025-06-27 16:05:14 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+
+import NextAuthSessionProvider from '../providers/sessionProvider';
+import 'styles/globals.css';
+
+export const metadata: Metadata = {
+  title: 'Next.js Boilerplate',
+  description: 'A simple boilerplate for next.js',
+  manifest: '/manifest.json'
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+      </body>
+    </html>
+  );
+}
